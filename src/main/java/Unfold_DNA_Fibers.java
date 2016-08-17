a03723bbf56c8e2a89c58a7c39d252059a344f4b
julien.pontabry <julien.pontabry@helmholtz-muenchen.de>
2016-08-17 11:12:41 +0200

    Initial and functional code

---
+++ src/main/java/Unfold_DNA_Fibers.java
@@ -0,0 +1,123 @@
+/*
+ * Manipulate and analyse DNA fibers data
+ * This plugin extracts and unfold the DNA fibers selected by a curve ROI
+ * Copyright (C) 2016  Julien Pontabry (Helmholtz IES)
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
+//import ij.gui.GenericDialog;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+
+/**
+ * UnfoldDNAFibers
+ *
+ * After DNA fibers detection (manually or automatically), the fibers are
+ * located by their centerline (represented by a line ROI). They can be
+ * unfolded (to make them straight) with this plugin.
+ *
+ * @author Julien Pontabry
+ */
+public class Unfold_DNA_Fibers implements PlugInFilter {
+	protected ImagePlus image;
+
+	// plugin parameters
+	public double value;
+	public String name;
+
+	/**
+	 * @see ij.plugin.filter.PlugInFilter#setup(java.lang.String, ij.ImagePlus)
+	 */
+	@Override
+	public int setup(String arg, ImagePlus imp) {
+		image = imp;
+		return DOES_8G | DOES_16 | DOES_32 | DOES_RGB;
+	}
+
+	/**
+	 * @see ij.plugin.filter.PlugInFilter#run(ij.process.ImageProcessor)
+	 */
+	@Override
+	public void run(ImageProcessor ip) {
+		/*if (showDialog()) {
+			process(ip);
+			image.updateAndDraw();
+		}*/
+	}
+
+	/*private boolean showDialog() {
+		GenericDialog gd = new GenericDialog("Process pixels");
+
+		// default value is 0.00, 2 digits right of the decimal point
+		gd.addNumericField("value", 0.00, 2);
+		gd.addStringField("name", "John");
+
+		gd.showDialog();
+		if (gd.wasCanceled())
+			return false;
+
+		// get entered values
+		value = gd.getNextNumber();
+		name = gd.getNextString();
+
+		return true;
+	}*/
+
+	/**
+	 * Process an image.
+	 *
+	 * Please provide this method even if {@link ij.plugin.filter.PlugInFilter} does require it;
+	 * the method {@link ij.plugin.filter.PlugInFilter#run(ij.process.ImageProcessor)} can only
+	 * handle 2-dimensional data.
+	 *
+	 * If your plugin does not change the pixels in-place, make this method return the results and
+	 * change the {@link #setup(java.lang.String, ij.ImagePlus)} method to return also the
+	 * <i>DOES_NOTHING</i> flag.
+	 *
+	 * @param image the image (possible multi-dimensional)
+	 */
+	public void process(ImagePlus image) {
+		IJ.showMessage("Hello world from new plugin!");
+	}
+
+	/**
+	 * Main method for debugging.
+	 *
+	 * For debugging, it is convenient to have a method that starts ImageJ, loads an
+	 * image and calls the plugin, e.g. after setting breakpoints.
+	 *
+	 * @param args unused
+	 */
+	public static void main(String[] args) {
+		// set the plugins.dir property to make the plugin appear in the Plugins menu
+		Class<?> clazz = Unfold_DNA_Fibers.class;
+		String url = clazz.getResource("/" + clazz.getName().replace('.', '/') + ".class").toString();
+		String pluginsDir = url.substring("file:".length(), url.length() - clazz.getName().length() - ".class".length());
+		System.setProperty("plugins.dir", pluginsDir);
+
+		// start ImageJ
+		new ImageJ();
+
+		/*// open the Clown sample
+		ImagePlus image = IJ.openImage("http://imagej.net/images/clown.jpg");
+		image.show();*/
+
+		/*// run the plugin
+		IJ.runPlugIn(clazz.getName(), "");*/
+	}
+}
